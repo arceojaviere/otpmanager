931fc8ab36f2de4d1e9c23356d950f012b73b1c7
Javier Arce <arceojaviere@yahoo.com.mx>
2018-09-18 16:44:24 -0500

    Adding tray icon using deprecated StatusIcon GTK component. Needs cleanup and restructuring of the token list to allow both the context menu and GUI boxes to correctly coexist and remain synchronized

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from otpmanager.view.mainview import MainWindow
+from otpmanager.view.mainview import StatusIcon
+
 import gi
 gi.require_version("Gtk", "3.0")
 from gi.repository import Gtk
 
 from data.model import Token
 
+
 window = MainWindow()
+status_icon = StatusIcon(window)
 
 window.show()
 
-window.connect("destroy", Gtk.main_quit)
 Gtk.main()
--- view/mainview.py
+++ view/mainview.py
@@ -1,6 +1,8 @@
 import gi
 gi.require_version("Gtk", "3.0")
+gi.require_version("AppIndicator3", "0.1")
 from gi.repository import Gtk,Gdk
+#from gi.repository import AppIndicator3
 
 from otpmanager.otp.generator import OTPGenerator
 from otpmanager.otp.uri import UriParser
@@ -9,6 +11,7 @@ from otpmanager.data.model import Token
 
 
 import datetime
+
 
 class MainWindow(Gtk.Window):
   otpGenerator = OTPGenerator()
@@ -20,6 +23,7 @@ class MainWindow(Gtk.Window):
     self.set_default_size(250,150)
     self._addComponents(self.otpDatabase.getTokens())
     self.show_all()
+    self.connect("destroy", Gtk.main_quit)
 
   def _addComponents(self, tokens):
     topBox = Gtk.Box(orientation = Gtk.Orientation.VERTICAL, spacing = 12)
@@ -30,6 +34,7 @@ class MainWindow(Gtk.Window):
     topBox.add(self._createTokensWindow(tokens))
         
     self.add(topBox)
+
     
   def _createPanelBox(self):
     panelBox = Gtk.Box(orientation = Gtk.Orientation.HORIZONTAL, spacing = 12)
@@ -63,7 +68,7 @@ class MainWindow(Gtk.Window):
     self.otpDatabase.addToken(token)
     self._addTokenBox(token)
     self.show_all()
-    
+
 
 class TokenBox(Gtk.Box):
   
@@ -100,3 +105,42 @@ class TokenBox(Gtk.Box):
     otp = self.otpManager.otpGenerator.newHotp(self._token.secret, count)
     self._clipboard.set_text(otp, -1)
     self._otpEntry.set_text(otp)
+
+
+class StatusIcon:
+  def __init__(self, window):
+    
+    self._window = window
+    
+    self.status_icon = Gtk.StatusIcon()
+    self.status_icon.set_from_icon_name("gcr-key-pair")
+
+    self.status_icon.connect("activate", self.toggle_window_visibility)
+    self.status_icon.connect("popup-menu", self.show_menu)
+
+ 
+   # create a menu
+    self.menu = Gtk.Menu()
+ 
+    # create items for the menu - labels, checkboxes, radio buttons and images are supported:
+       
+    item = Gtk.MenuItem("Quit")
+    item.show()
+    item.connect("activate", self.quit)
+    self.menu.append(item)
+                   
+    self.menu.show()
+ 
+#    self.status_icon.set_menu(self.menu)
+
+  def show_menu(self, status_icon, button, activate_time):
+    self.menu.popup(None, None, None, Gtk.StatusIcon.position_menu, button, activate_time)
+
+  def toggle_window_visibility(self, status_icon):
+    if self._window.is_visible():
+      self._window.hide()
+    else:
+      self._window.show()
+
+  def quit(self, widget, data=None):
+    Gtk.main_quit()
